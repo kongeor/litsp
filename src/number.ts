7f779c2774f6b7f898e0fbd5fc4a5084ebdc6be3
Kostas Georgiadis <kongeor@gmail.com>
2017-01-11 15:39:42 +0200

    Create numbers

---
+++ src/number.ts
@@ -0,0 +1,24 @@
+import Environment from './environment';
+import { Eval, Egal } from './interface';
+
+export class Num implements Eval, Egal {
+
+    static readonly REGEX = /^\d+$/;
+
+    data: number;
+
+    constructor(data: number) {
+        this.data = data;
+    }
+
+    eval(env: Environment, args = []) {
+        return this;
+    }
+
+    equals(rhs) {
+        if (rhs instanceof Num) {
+            return this.data === rhs.data;
+        }
+        return false;
+    }
+}
